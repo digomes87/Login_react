2aaf3f18eeacd763ef32fc8145716636fb5bf8c8
Diego Go <diego.gomes87@gmail.com>
2019-09-07 12:10:30 -0300

    Gostei de trabalhar com React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Navbar } from './components/layout/NavBar'
 
 class App extends Component{
   render() {
+    const Nome = 'Diego';
+    const Carregando = false;
+
+
+    // const Carregando = false ? "Jogo esta Carregando":"Jogo foi cancelado";
+
+    
+
     return (
       <div className="App">
-        <h1>Aqui é React</h1>
+        {/* <h1>Aqui é React {Nome} e o, {Carregando ? "Carregando":"Vai pra casa"}</h1> */}
+      
+
+      <Navbar />
       </div>
     );
   }
